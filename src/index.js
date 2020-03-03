59059a5beff5bf8da7cd152fa74b531ac02cc9db
Timur Kalimullin <timur_kalimullin@list.ru>
2020-03-04 04:04:18 +0500

    feat: task resolve 20/20 test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ module.exports = function check(str, bracketsConfig) {
     let  obj = Object.fromEntries(arguments[1]),
         openBracket = Object.keys(Object.fromEntries(arguments[1])),
         closeBracket = Object.values(Object.fromEntries(arguments[1])),
+        sameBracket = openBracket.map((el,i)=>el==closeBracket[i]?el:null).filter(el=>el),
         temp = [];
 
     str = str.split('');
@@ -11,6 +12,8 @@ module.exports = function check(str, bracketsConfig) {
             temp.push(str[i]);
         } else if (closeBracket.includes(str[i]) && !openBracket.includes(str[i])) {
             if (obj[temp[temp.length-1]] == str[i]) {temp.pop()} else {return false};
+        } else if (sameBracket.includes(str[i])) {
+            if (temp[temp.length-1] != str[i]) {temp.push(str[i])} else {temp.pop()}
         }
     }
     if (temp.length!=0) {return false}
