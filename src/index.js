0a4993d144d90c2a8d60caacf62e665719918811
Timur Kalimullin <timur_kalimullin@list.ru>
2020-03-04 02:51:03 +0500

    feat: task 17/20 test pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let  obj = Object.fromEntries(arguments[1]),
+        openBracket = Object.keys(Object.fromEntries(arguments[1])),
+        closeBracket = Object.values(Object.fromEntries(arguments[1])),
+        temp = [];
+
+    str = str.split('');
+
+    for (let i=0;i<str.length;i++) {
+        if (openBracket.includes(str[i]) && !closeBracket.includes(str[i])) {
+            temp.push(str[i]);
+        } else if (closeBracket.includes(str[i]) && !openBracket.includes(str[i])) {
+            if (obj[temp[temp.length-1]] == str[i]) {temp.pop()} else {return false};
+        }
+    }
+    if (temp.length!=0) {return false}
+    return true;
 }
